b2191488b0c3bcb2d9ae29391c99ec7dff61a4ee
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-02-09 21:19:31 -0300

    information about the source path of a file

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,12 +10,12 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Enter a folder path: ");
-		String strPath = sc.nextLine();
+		String strPath1 = sc.nextLine();
 		
-		File path = new File(strPath);
+		File path1 = new File(strPath1);
 		
 		//listagem de todas as pastas/diretórios a partir do caminho passado.
-		File[] folders = path.listFiles(File::isDirectory);
+		File[] folders = path1.listFiles(File::isDirectory);
 		
 		System.out.println();
 		System.out.println("FOLDERS: ");
@@ -24,7 +24,7 @@ public class Program {
 		}
 		
 		//listagem de todos os arquivos a partir do caminho passado.
-		File[] files = path.listFiles(File::isFile);
+		File[] files = path1.listFiles(File::isFile);
 		
 		System.out.println();
 		System.out.println("FILES: ");
@@ -33,9 +33,26 @@ public class Program {
 		}
 		
 		//criando uma sub pasta a partir do caminho informado.
-		boolean sucess = new File(strPath + "\\SubPasta").mkdir();
+		boolean sucess = new File(strPath1 + "\\SubPasta").mkdir();
 		System.out.println();
 		System.out.println("Directory created sucessfully: " + sucess);
+		
+		
+		System.out.println();
+		System.out.println("Enter a file path: ");
+		String strPath2 = sc.nextLine();
+		
+		File path2 = new File(strPath2);
+		
+		System.out.println();
+		//obtendo informacoes sobre o nome de um arquivo
+		System.out.println("getName: " + path2.getName());
+		
+		//obtendo informacoes sobre o caminho de um arquivo (sem o nome do arquivo).
+		System.out.println("getParent: " + path2.getParent());
+		
+		//obtendo informacoes sobre o caminho completo de um arquivo.
+		System.out.println("getPath: " + path2.getPath());
 		
 		sc.close();
 	}
